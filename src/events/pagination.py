2a965ead5a44f3c2796bac5d636e396c178f4f80
Zhenya Sok <kapitan.kub@gmail.com>
2025-08-25 17:11:16 +0500

    alternative option (relative to the blog on habr)

---
+++ src/events/pagination.py
@@ -0,0 +1,6 @@
+from rest_framework.pagination import CursorPagination
+
+class EventCursorPagination(CursorPagination):
+    page_size = 10  # сколько элементов на странице
+    ordering = 'date'  # поле, по которому будет курсор (можно менять на '-date' для обратного порядка)
+    cursor_query_param = 'cursor'  # параметр в URL, например ?cursor=abc123
