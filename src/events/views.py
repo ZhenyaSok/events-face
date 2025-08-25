cb212c6ecb9ac67f8f2a9efc4024bce4c064d47b
Zhenya Sok <kapitan.kub@gmail.com>
2025-08-25 17:11:22 +0500

    alternative option (relative to the blog on habr)

--- src/events/views.py
+++ src/events/views.py
@@ -1,26 +1,48 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+#
+# from rest_framework import viewsets, filters
+# from django_filters.rest_framework import DjangoFilterBackend
+# from .models import Event
+# from .serializers import EventSerializer
+#
+#
+# class EventViewSet(viewsets.ReadOnlyModelViewSet):
+#     """
+#     Список мероприятий (только open), с фильтрацией, сортировкой и пагинацией.
+#     """
+#     serializer_class = EventSerializer
+#     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+#     filterset_fields = ["name"]  # фильтрация по названию
+#     search_fields = ["name"]     # поиск по названию (LIKE)
+#     ordering_fields = ["date"]   # сортировка по дате
+#     ordering = ["date"]          # сортировка по умолчанию
+#
+#     def get_queryset(self):
+#         return (
+#             Event.objects
+#             .filter(status=Event.Status.OPEN)
+#             .select_related("venue")  # решает проблему N+1
+#         )
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Event
 from .serializers import EventSerializer
+from .pagination import EventCursorPagination
 
 
 class EventViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    Список мероприятий (только open), с фильтрацией, сортировкой и пагинацией.
-    """
     serializer_class = EventSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
-    filterset_fields = ["name"]  # фильтрация по названию
-    search_fields = ["name"]     # поиск по названию (LIKE)
-    ordering_fields = ["date"]   # сортировка по дате
-    ordering = ["date"]          # сортировка по умолчанию
+    filterset_fields = ["name"]      # фильтрация по названию
+    search_fields = ["name"]         # поиск по названию
+    ordering_fields = ["date"]       # сортировка по дате
+    ordering = ["date"]              # сортировка по умолчанию
+    pagination_class = EventCursorPagination  # подключаем курсорную пагинацию
 
     def get_queryset(self):
         return (
             Event.objects
             .filter(status=Event.Status.OPEN)
-            .select_related("venue")  # решает проблему N+1
+            .select_related("venue")  # предотвращаем N+1
         )
 
