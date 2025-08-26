8fe6ee1599db9ddc2f9bfb866b9a9df813df3010
Zhenya Sok <kapitan.kub@gmail.com>
2025-08-26 12:05:51 +0500

    correctly settings of celery

--- celery.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "events_face.settings")
-app = Celery("events_face")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
